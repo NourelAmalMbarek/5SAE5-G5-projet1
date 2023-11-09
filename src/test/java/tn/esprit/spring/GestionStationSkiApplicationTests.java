65605ba45ff532fcf93028ede903453458a45c02
unknown <naoures.elair@esprit.tn>
2023-11-09 12:57:03 +0100

    dockcompose

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,9 +1,8 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class GestionStationSkiApplicationTests {
 
 	@Test
